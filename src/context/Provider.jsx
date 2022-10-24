5935e7d3a9a3f70ae0cf56a6ce1e15079e47fe5d
iamlucasgomes <lucas.devjs@gmail.com>
2022-10-24 17:48:52 -0300

    Update: Chamada da API e salva no estado global

---
+++ src/context/Provider.jsx
@@ -0,0 +1,70 @@
+import PropTypes from 'prop-types';
+import { useEffect, useMemo, useState } from 'react';
+import { fetchAPI } from '../services/fetchApi';
+import context from './context';
+
+function Provider({ children }) {
+  const [categoriasFood, setcategoriasFood] = useState([]);
+  const [nacionalidadeFood, setNacionalidadeFood] = useState([]);
+  const [foods, setFoods] = useState([]);
+  const [ingredientesFood, setIngredientesFood] = useState([]);
+  const [categoriasDrink, setcategoriasDrink] = useState([]);
+  const [drink, setDrinks] = useState([]);
+  const [ingredientesDrink, setIngredientesDrink] = useState([]);
+
+  useEffect(() => {
+    const fetchMeal = async () => {
+      const fetchCategories = await fetchAPI('themealdb', 'list.php?c', 'list');
+      const fetchNacionalidade = await fetchAPI('themealdb', 'list.php?a', 'list');
+      const fetchFoods = await fetchAPI('themealdb', 'search.php?s', '');
+      const fetchIngredientes = await fetchAPI('themealdb', 'list.php?i', 'list');
+
+      setcategoriasFood(fetchCategories.meals);
+      setNacionalidadeFood(fetchNacionalidade.meals);
+      setFoods(fetchFoods.meals);
+      setIngredientesFood(fetchIngredientes.meals);
+    };
+    const fetchDrink = async () => {
+      const fetchCategories = await fetchAPI('thecocktaildb', 'list.php?c', 'list');
+      const fetchDrinks = await fetchAPI('thecocktaildb', 'search.php?s', '');
+      const fetchIngredientes = await fetchAPI('thecocktaildb', 'list.php?i', 'list');
+
+      setcategoriasDrink(fetchCategories.drinks);
+      setDrinks(fetchDrinks.drinks);
+      setIngredientesDrink(fetchIngredientes.drinks);
+    };
+
+    fetchMeal();
+    fetchDrink();
+  }, []);
+
+  const contextValue = useMemo(
+    () => ({
+      categoriasFood,
+      nacionalidadeFood,
+      foods,
+      ingredientesFood,
+      categoriasDrink,
+      drink,
+      ingredientesDrink,
+    }),
+    [categoriasDrink,
+      categoriasFood,
+      drink,
+      foods,
+      ingredientesDrink,
+      ingredientesFood,
+      nacionalidadeFood],
+  );
+  return (
+    <context.Provider value={ contextValue }>
+      {children}
+    </context.Provider>
+  );
+}
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Provider;
