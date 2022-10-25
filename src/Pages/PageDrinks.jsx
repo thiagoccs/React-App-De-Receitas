4f73862576c358bc87dc9c35ec53ccbac65a3bab
Luis Alberto H. Cardoso <luis.alberto3556@gmail.com>
2022-10-25 16:07:27 -0300

    reqs footer

---
+++ src/Pages/PageDrinks.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Footer from '../components/Footer';
+
+export default function PageDrink() {
+  return (
+    <>
+      <div>PageDrink</div>
+      <Footer />
+    </>
+  );
+}
--- src/Pages/PageMeals.jsx
+++ src/Pages/PageMeals.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Footer from '../components/Footer';
 
 export default function PageMeals() {
   return (
-    <div>PageMeals</div>
+    <>
+      <div>PageMeals</div>
+      <Footer />
+    </>
   );
 }
