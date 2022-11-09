5d714ca1a07b2826ee46e609734c45e1eed2e022
iamlucasgomes <lucas.devjs@gmail.com>
2022-11-09 13:27:11 -0300

    perf & feat: Salve as receitas favoritas no `localStorage` na chave `favoriteRecipes, Implemente o ícone do coração (favorito) de modo que: deve vir preenchido caso a receita esteja favoritada e "despreenchido" caso contrário, alteração na função que salva o favoriteRecipe no localStorage para aumento de performance`

--- src/components/ShareAndFavoriteBtn.jsx
+++ src/components/ShareAndFavoriteBtn.jsx
@@ -1,48 +1,95 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import copy from 'clipboard-copy';
-import { useParams, useHistory } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import context from '../context/context';
+import LinkCopied from './LinkCopied';
+import shareIcon from '../images/shareIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import setFavoriteRecipes from '../func/setFavoriteRecipes';
 
 function ShareAndFavoriteBtn() {
-  const { mealsDetailsState, drinksDetailsState } = useContext(context);
+  const { mealsDetailsState,
+    setMealsDetailsState,
+    drinksDetailsState,
+    setDrinksDetailsState } = useContext(context);
   const [isLinkCopied, setIsLinkCopied] = useState(false);
+  const [favoriteHeart, setFavoriteHeart] = useState(false);
   const { id } = useParams();
-  const { location: { pathname } } = useHistory();
+  const { pathname } = useLocation();
 
   function handleClickShare(type, param) {
     setIsLinkCopied(true);
     copy(`http://localhost:3000/${type}/${param}`);
   }
 
-  const localStorageInfo = () => {
-    if (pathname.includes('meals')) {
-      const dataMeals = mealsDetailsState.meals
-        .map((meal) => ({
-          id: meal.idMeal,
-          type: 'meal',
-          nationality: meal.strArea,
-          category: meal.strCategory,
-          alcoholicOrNot: '',
-          name: meal.strMeal,
-          image: meal.strMealThumb,
-        }));
-      console.log(dataMeals);
-      localStorage.setItem('favoriteRecipes', JSON.stringify(dataMeals));
-    }
-    if (pathname.includes('drinks')) {
-      const dataDrinks = drinksDetailsState.drinks
-        .map((drink) => ({
-          id: drink.idDrink,
-          type: 'drink',
-          nationality: drink.strArea || '',
-          category: drink.strCategory,
-          alcoholicOrNot: drink.strAlcoholic || '',
-          name: drink.strDrink,
-          image: drink.strDrinkThumb,
-        }));
-      localStorage.setItem('favoriteRecipes', JSON.stringify(dataDrinks));
-    }
+  // const localStorageInfo = () => {
+  //   if (pathname.includes('meals')) {
+  //     const dataMeals = mealsDetailsState.meals
+  //       .map((meal) => ({
+  //         id: meal.idMeal,
+  //         type: 'meal',
+  //         nationality: meal.strArea,
+  //         category: meal.strCategory,
+  //         alcoholicOrNot: '',
+  //         name: meal.strMeal,
+  //         image: meal.strMealThumb,
+  //       }));
+  //     console.log(dataMeals);
+  //     localStorage.setItem('favoriteRecipes', JSON.stringify(dataMeals));
+  //   }
+  //   if (pathname.includes('drinks')) {
+  //     const dataDrinks = drinksDetailsState.drinks
+  //       .map((drink) => ({
+  //         id: drink.idDrink,
+  //         type: 'drink',
+  //         nationality: drink.strArea || '',
+  //         category: drink.strCategory,
+  //         alcoholicOrNot: drink.strAlcoholic || '',
+  //         name: drink.strDrink,
+  //         image: drink.strDrinkThumb,
+  //       }));
+  //     localStorage.setItem('favoriteRecipes', JSON.stringify(dataDrinks));
+  //   }
+  // };
+
+  const handleFavorite = (snack) => {
+    setFavoriteHeart((prevState) => !prevState);
+    console.log(snack);
+    setFavoriteRecipes(snack, id, pathname);
   };
+  useEffect(() => {
+    const favoriteStorage = JSON.parse(localStorage
+      .getItem('favoriteRecipes'));
+    if (favoriteStorage !== null && favoriteStorage.some((item) => item.id === id)) {
+      setFavoriteHeart(true);
+    }
+  }, [id]);
+
+  // useEffect(() => {
+  //   const fetchMealsDetails = async () => {
+  //     const mealsDetails = await fetchAPI(
+  //       'themealdb',
+  //       'lookup.php?i',
+  //       id,
+  //     );
+  //     setMealsDetailsState(mealsDetails.meals[0]);
+  //   };
+  //   const fetchDrinksDetails = async () => {
+  //     const drinksDetails = await fetchAPI(
+  //       'thecocktaildb',
+  //       'lookup.php?i',
+  //       id,
+  //     );
+  //     setDrinksDetailsState(drinksDetails.drinks[0]);
+  //   };
+  //   if (pathname.includes('meals')) {
+  //     fetchMealsDetails();
+  //   }
+  //   if (pathname.includes('drinks')) {
+  //     fetchDrinksDetails();
+  //   }
+  // }, [id, pathname]);
 
   return (
     <div>
@@ -53,18 +100,26 @@ function ShareAndFavoriteBtn() {
           ? () => handleClickShare('meals', id)
           : () => handleClickShare('drinks', id) }
       >
-        Share
+        <img src={ shareIcon } alt="share" />
       </button>
       <br />
       <button
         type="button"
-        data-testid="favorite-btn"
-        onClick={ localStorageInfo }
+        onClick={ () => (pathname
+          .includes('meals') ? handleFavorite(mealsDetailsState.meals[0])
+          : handleFavorite(drinksDetailsState.drinks[0])) }
       >
-        Favorite
+        {' '}
+        <img
+          data-testid="favorite-btn"
+          src={
+            favoriteHeart ? blackHeartIcon : whiteHeartIcon
+          }
+          alt="favorite"
+        />
       </button>
 
-      {isLinkCopied && <p>Link copied!</p>}
+      {isLinkCopied && <LinkCopied />}
 
     </div>
   );
